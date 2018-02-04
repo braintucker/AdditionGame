46f92a666ce6282b53350dccb8d2f55170414edb
Brain Tucker <brian.briantucker@gmail.com>
2018-02-04 11:11:43 -0600

    buttons now update when answer is chosen, timer doesn't function yet

--- app/src/main/java/com/briantucker/additiongame/MainActivity.java
+++ app/src/main/java/com/briantucker/additiongame/MainActivity.java
@@ -13,51 +13,20 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    Button btn0;
+    Button btn1;
+    Button btn2;
+    Button btn3;
     ArrayList<Integer> answers = new ArrayList<Integer>();
     TextView resultTextView;
     TextView ptsTextView;
+    TextView sumTextView;
     int locationOfCorrectAnswer;
     int score = 0;
     int numberOfQuestions = 0;
 
 
-    public void chooseAnswer(View view) {
-
-
-        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
-            score++;
-            resultTextView.setText("Correct!");
-            Log.i("correct", "correct answer!");
-        }
-        else{
-            resultTextView.setText("Wrong!");
-        }
-
-        numberOfQuestions++;
-        ptsTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
-    }
-
-    public void start(View view){
-
-        startButton.setVisibility(View.INVISIBLE);
-
-
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startButton = (Button)findViewById(R.id.startButton);
-
-        TextView sumTextView = (TextView)findViewById(R.id.sumTextView);
-        Button btn0 = (Button)findViewById(R.id.btn0);
-        Button btn1 = (Button)findViewById(R.id.btn1);
-        Button btn2 = (Button)findViewById(R.id.btn2);
-        Button btn3 = (Button)findViewById(R.id.btn3);
-        resultTextView = (TextView)findViewById(R.id.resultTextView);
-        ptsTextView = (TextView) findViewById(R.id.ptsTextView);
+    public void generateQuestion(){
 
         Random rand = new Random();
 
@@ -67,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
 
         locationOfCorrectAnswer = rand.nextInt(4);
+
+        answers.clear();
 
         int incorrectAnswer;
 
@@ -90,6 +61,48 @@ public class MainActivity extends AppCompatActivity {
         btn1.setText(Integer.toString(answers.get(1)));
         btn2.setText(Integer.toString(answers.get(2)));
         btn3.setText(Integer.toString(answers.get(3)));
+    }
+
+
+    public void chooseAnswer(View view) {
+
+
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            score++;
+            resultTextView.setText("Correct!");
+        }
+        else{
+            resultTextView.setText("Wrong!");
+        }
+
+        numberOfQuestions++;
+        ptsTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+        generateQuestion();
+    }
+
+    public void start(View view){
+
+        startButton.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.startButton);
+        sumTextView = (TextView)findViewById(R.id.sumTextView);
+        btn0 = (Button)findViewById(R.id.btn0);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        resultTextView = (TextView)findViewById(R.id.resultTextView);
+        ptsTextView = (TextView) findViewById(R.id.ptsTextView);
+
+        generateQuestion();
+
 
     }
 }
